c36d7746d6c342eac02c0cef88ac9fb0e8bb72f1
Rhelli <rory.hellier@googlemail.com>
2021-07-15 18:16:03 +0100

    Creates And Sets Up Webpack/React App:
     - Creates babelrc config file
     - Removes /dist/index.html
     - Modifies the package.json file to re-add main and remove the private declaration
     - Adds new src/index.html file for React output point
     - Adds new src/index.js file for React output point
     - Adds new src/index.scss with basic page configuration
     - Modifies webpack.config.js to include new config for sass, image loaders, file-loader, url-loader, postcss-loader, path resolving and output config

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Welcome To Thrive</h1>
+    <button>Login</button>
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
