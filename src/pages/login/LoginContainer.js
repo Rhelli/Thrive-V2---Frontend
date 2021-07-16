b86062e0a66467412ee92d34ab43ac7656d2fb12
Rhelli <rory.hellier@googlemail.com>
2021-07-16 19:42:10 +0100

    Modifies The LoginContainer To Render Simple Layout, Renames LoginStore to loginState

--- src/pages/login/LoginContainer.js
+++ src/pages/login/LoginContainer.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 const LoginContainer = () => {
-  
+
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </div>
+  )
 }
