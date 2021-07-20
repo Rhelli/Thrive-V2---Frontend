32acf7cc5b2b28bde4e5e5efebad8ecc711f7031
Rhelli <rory.hellier@googlemail.com>
2021-07-20 18:51:53 +0100

    Removes Last SCSS Module Declaration From Navbar.jsx

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import MainNavbar from '../MainNavbar/MainNavbar';
 import AuthNavbar from '../AuthNavbar/AuthNavbar';
-import styles from './Navbar.module.scss';
 
 const Navbar = () => (
-  <div className={style.Navbar}>
+  <div>
     <nav>
       <div>
         <MainNavbar />
